1a6bb228b7fa9d80797ac650e623856ed3b0c21d
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2015-10-08 14:00:46 -0700

    Problem Set 4 Exercise #2

---
+++ EvenThough.java
@@ -0,0 +1,21 @@
+
+package eventhough;
+
+/*
+ * @author Filip Kuzmanovski
+ */
+public class EvenThough {
+    public static void main(String[] args) {
+        
+        // declaring initial variable
+       int evenNum = 2;
+       
+       // while loop for printing even numbers up to 100
+       while (evenNum <= 100) 
+       {
+          System.out.print(" " + evenNum);
+          evenNum += 2;
+       }
+    }
+    
+}
