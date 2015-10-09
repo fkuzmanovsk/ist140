caab0b897a0e79ae60acc688c733d2564316c69e
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2015-10-08 18:23:10 -0700

    Added Unit 4 Problem Set Exercise 5

---
+++ MultiTable.java
@@ -0,0 +1,18 @@
+package multitable;
+
+/*
+ * @author Filip Kuzmanovski
+ */
+public class MultiTable {
+
+    public static void main(String[] args) {
+    	
+        // For loop for creating a Multiplication table
+        for (int i=1;i<=9;i++){
+            for (int j=1;j<=9;j++)
+            System.out.print("\t"+i*j);
+            
+        System.out.println(); 
+}
+}   
+}
