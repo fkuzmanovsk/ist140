1f4bea662e610ae91c607a55611e1c54d03871c8
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2015-10-22 19:34:14 -0700

    MinMax pushed

---
+++ MinMax.java
@@ -0,0 +1,18 @@
+
+package minmax;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kuzman
+ */
+public class MinMax {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
