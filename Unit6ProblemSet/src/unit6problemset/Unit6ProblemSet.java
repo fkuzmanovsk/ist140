685bf3051b116b125b4d8f1ec3aa5f601b98d020
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2015-11-11 08:02:17 -0800

    productBuilder method added to Unit 6 Problem Set

--- Unit6ProblemSet/src/unit6problemset/Unit6ProblemSet.java
+++ Unit6ProblemSet/src/unit6problemset/Unit6ProblemSet.java
@@ -4,24 +4,39 @@
  * and open the template in the editor.
  */
 package unit6problemset;
+import java.util.Scanner;
 
-/**
- *
- *
+/*
+ @author Filip Kuzmanovski
  */
 public class Unit6ProblemSet {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         // Call your methods here
         
     }
     
+  
     
     // Write your methods below here
- 
+     // banner message 
+    public static void bannerPrinter() {
+        System.out.println("******************************************");
+        System.out.println("****** Welcome to my eCommerce app! ******");
+        System.out.println("******************************************");
+        System.out.println(""); //instructions requested blank line after banner
+    }
+    
+    /* Write a method called productBuilder that takes no parameters and returns an array of Strings.
+    In the method body construct and return an array of 5 Strings (call the array products)
+    and populate the array with 5 product names. You don't need to get the values from the user, just 
+    populate with 5 product names. */
+    public static String[] productBuilder() {
+        String[] products = {"PS4","iPhone6S","XBOX ONE", "Macbook Pro", "iPod"};
+        return products;
+    }
+    
+    
     
 }
